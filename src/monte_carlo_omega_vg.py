9dfce73fcdbd2bf54b6f5c2ef795f578f2c5db73
ryutau <ryumorimori0609@gmail.com>
2021-09-09 18:00:05 +0900

    update garch & make script to generate log-log plot

--- src/monte_carlo_omega_vg.py
+++ src/monte_carlo_omega_vg.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import re
 import time
+from collections import defaultdict
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -18,39 +19,30 @@ script_name = re.sub(r"\.py$", "", os.path.basename(__file__))
 def main(omega):
     start = time.time()
     ts_kind_list = [
-        "White noise",
-        "Chaotic logistic map",
-        "Unbiased additive random walk",
-        "Additive random walk with positive drift",
-        "Unbiased additive random walk with memory",
-        "Unbiased multiplicative random walk",
+        # "White noise",
+        # "Chaotic logistic map",
+        # "Unbiased additive random walk",
+        # "Additive random walk with positive drift",
+        # "Unbiased additive random walk with memory",
+        # "Unbiased multiplicative random walk",
         "Multiplicative random walk with negative drift",
-        "Multiplicative random walk with volatility clustering (GARCH)",
+        # "Multiplicative random walk with volatility clustering (GARCH)",
     ]
     n_iter = 10
-    max_power_idx = 17  # max size of the time series will be 2**max_power_idx
+    max_power_idx = 16  # max size of the time series will be 2**max_power_idx
     for ts_kind in ts_kind_list:
-        whole_graph_list = [
-            EfficientVisibilityGraph(
+        result_dict = defaultdict(list)
+        for i in range(n_iter):
+            print(i)
+            vg = EfficientVisibilityGraph(
                 generate_ts(kind=ts_kind, size=2 ** max_power_idx, seed=i),
                 name="",
                 window_width=omega,
             )
-            for i in range(n_iter)
-        ]
-        result_df = pd.DataFrame(
-            {
-                N: [
-                    BasicSubGraph(vg, N).compute_irreversibility()
-                    for vg in whole_graph_list
-                ]
-                for N in [2 ** i for i in range(5, max_power_idx + 1)]
-            }
-        )
-        result_df.to_csv(
-            f"{save_dir(script_name)}"
-            f"/original_vg-{omega}_mc_result_{ts_kind}.csv"
-        )
+            for N in [2 ** j for j in range(5, max_power_idx + 1)]:
+                result_dict[N].append(BasicSubGraph(vg, N).compute_irreversibility())
+        result_df = pd.DataFrame(result_dict)
+        result_df.to_csv(f"{save_dir(script_name)}" f"/original_vg-{omega}_mc_result_{ts_kind}.csv")
         print(f"{ts_kind} has been finished.")
     elapsed_time = time.time() - start
     print(f"elapsed_time:{elapsed_time}[sec]")
---
+++ src/save_log_log_plot_on_synthetic_series.py
@@ -0,0 +1,86 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.gridspec import GridSpec, GridSpecFromSubplotSpec
+from tools.save import save_dir
+
+plt.rcParams["font.size"] = 15
+plt.rcParams["axes.formatter.use_mathtext"] = True
+
+ts_kind_list = [
+    "White noise",
+    "Chaotic logistic map",
+    "Unbiased additive random walk",
+    "Additive random walk with positive drift",
+    "Unbiased additive random walk with memory",
+    "Unbiased multiplicative random walk",
+    "Multiplicative random walk with negative drift",
+    "Multiplicative random walk with volatility clustering (GARCH)",
+]
+
+name_list = [
+    "White noise",
+    "Chaotic logistic map",
+    "Unbiased ARW",
+    "ARW nwith positive drift",
+    "Unbiased ARW with memory",
+    "Unbiased MRW",
+    "MRW with negative drift",
+    "GARCH",
+]
+
+name_mapper = {kind: name for kind, name in zip(ts_kind_list, name_list)}
+
+
+def read_results(ts_kind):
+    vg_data = pd.read_csv(
+        f"../output/monte_carlo_omega_vg/original_vg-None_mc_result_{ts_kind}.csv",
+        index_col=0,
+    )
+    lvg_data = pd.read_csv(
+        f"../output/monte_carlo_omega_vg/original_vg-100_mc_result_{ts_kind}.csv",
+        index_col=0,
+    )
+    dvg_data = pd.read_csv(
+        f"../output/monte_carlo_refined_vg/deg-vec_vg-2_mc_result_{ts_kind}.csv",
+        index_col=0,
+    )
+    return [vg_data, lvg_data, dvg_data]
+
+
+def visualize_three_measures(ts_kind_list, name_mapper, figure_title):
+    n = len(ts_kind_list)
+    results_dic = {ts_kind: read_results(ts_kind) for ts_kind in ts_kind_list}
+    title_list = ["VG", "LVG", "DVG"]
+    color_list = ["#7249F5", "#3CA832", "#FF690D"]
+    figure = plt.figure(figsize=(6.2 * n, 5))
+    gs_master = GridSpec(nrows=1, ncols=n)
+    plot_space = GridSpecFromSubplotSpec(nrows=1, ncols=n, subplot_spec=gs_master[0, :])
+
+    for i, (ts_kind, result) in enumerate(results_dic.items()):
+        figure.add_subplot(plot_space[:, i])
+        plt.title(name_mapper[ts_kind], fontsize=22, pad=10)
+        plt.ylim(1e-6, 20)
+        plt.xticks(rotation=45)
+        plt.yscale("log")
+        plt.xscale("log")
+        plt.xlabel("Time series length: $N$")
+        factor = 1
+        for j, (data, title, color) in enumerate(zip(result, title_list, color_list)):
+            mean = data.mean()
+            eb = plt.errorbar(
+                data.columns.astype(int) * factor,
+                mean,
+                yerr=[mean - data.quantile(0.1), data.quantile(0.9) - mean],
+                c=color,
+                capsize=4,
+                label=title,
+                lw=1.6,
+                elinewidth=1.3,
+            )
+            eb[-1][0].set_linestyle("--")
+            factor *= 1.08
+        if i == 0:
+            plt.ylabel("KLD")
+            plt.legend(loc="lower left")
+    plt.savefig(f"{save_dir('figures_for_paper')}/log-log_plot_{figure_title}.png")
+    plt.close()
